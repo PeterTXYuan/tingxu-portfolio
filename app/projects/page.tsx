c5bf058f4e9813856eb44b4cc0956a01d8abec2c
tyuan2 <tyuan2@uw.edu>
2025-08-20 22:19:17 +0800

    feat: first commit with simple project page

---
+++ app/projects/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+import Content from "@/content/projects/first.mdx";
+
+export default function ProjectsPage() {
+  return (
+    <main className="prose mx-auto p-6">
+      <Content />
+    </main>
+  );
+}
